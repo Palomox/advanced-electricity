923e56e0f0415f6bc710e1079da4cbdf85d61d1e
Palomox <cocelpalomox@gmail.com>
2020-05-19 18:30:37 +0200

    Temporary solution to the error - to be solver

--- src/main/java/com/github/palomox/advancedelectricity/init/ModRecipeSerializers.java
+++ src/main/java/com/github/palomox/advancedelectricity/init/ModRecipeSerializers.java
@@ -12,6 +12,6 @@ import net.minecraftforge.registries.ObjectHolder;
 
 @ObjectHolder(AdvancedElectricity.MODID)
 public class ModRecipeSerializers {
-	public static IRecipeSerializer<AdvancedTableShaped> advt_shaped = Registry.register(Registry.RECIPE_SERIALIZER, new ResourceLocation("advt_shaped"), new ModRecipeSerializers()); 
-	public static IRecipeType<AdvancedTableShaped> advt_shapedr = Registry.register(Registry.RECIPE_TYPE, new ResourceLocation("advt_shapedr"), new ModRecipeSerializers());
+	public static IRecipeSerializer<AdvancedTableShaped> advt_shaped = null; // Registry.register(Registry.RECIPE_SERIALIZER, new ResourceLocation("advt_shaped"), new ModRecipeSerializers()); 
+	public static IRecipeType<AdvancedTableShaped> advt_shapedr = null; //Registry.register(Registry.RECIPE_TYPE, new ResourceLocation("advt_shapedr"), new ModRecipeSerializers());
 }
